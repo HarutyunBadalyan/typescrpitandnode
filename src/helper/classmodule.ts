bdb267f5ce42711adb8ece93b953eb3c8a85ad33
HarutyunBadalyan <harutyun.badalyan@inomma.com>
2022-02-22 15:46:54 +0400

    delete height property in User model

--- src/helper/classmodule.ts
+++ src/helper/classmodule.ts
@@ -2,12 +2,12 @@ class User {
     name:string;
     surName: string;
     age: number;
-    height: number;
-    constructor(name:string, surName:string, age: number, height: number) {
+   
+    constructor(name:string, surName:string, age: number, ) {
         this.name = name;
         this.surName = surName;
         this.age = age;
-        this.height = height;
+        
     }
 }
 export default User;
