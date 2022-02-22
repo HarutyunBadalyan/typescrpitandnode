a3fa59a1df97ba4f6b6d556c296261c54ad5b7b9
HarutyunBadalyan <harutyun.badalyan@inomma.com>
2022-02-22 15:39:19 +0400

    created User class module

---
+++ src/helper/classmodule.ts
@@ -0,0 +1,9 @@
+class User {
+    name:string;
+    surName: string;
+    constructor(name:string, surName:string) {
+        this.name = name;
+        this.surName = surName;
+    }
+}
+export default User;
--- src/router/somethingroute.ts
+++ src/router/somethingroute.ts
@@ -1,8 +1,9 @@
 import express, { Response, Request } from 'express';
 
-
+import User from '../helper/classmodule';
 const someThingRoute = express.Router();
 someThingRoute.get('/something', (req: Request | any, res: Response | any) => {
+    console.log(User)
   console.log("something1");
   console.log("something2");
   console.log("something 3")
