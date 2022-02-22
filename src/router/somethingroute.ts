dc067f3371a55139547a9d3540a25f97edf0e16d
HarutyunBadalyan <harutyun.badalyan@inomma.com>
2022-02-22 13:05:54 +0400

    something3

--- src/router/somethingroute.ts
+++ src/router/somethingroute.ts
@@ -4,7 +4,8 @@ import express, { Response, Request } from 'express';
 const someThingRoute = express.Router();
 someThingRoute.get('/something', (req: Request | any, res: Response | any) => {
   console.log("something1");
-  console.log("something2")
+  console.log("something2");
+  console.log("something 3")
     res.send('something');
 });
 export default someThingRoute;
