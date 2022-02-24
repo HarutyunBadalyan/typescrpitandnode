d1f7401122de79470136b14a46f243f6157d58ec
HarutyunBadalyan <harutyun.badalyan@inomma.com>
2022-02-24 17:51:32 +0400

    build class module with js

---
+++ build/helper/classmodulewithjs.js
@@ -0,0 +1,10 @@
+"use strict";
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    static method() {
+        console.log("dfgdfgdfgdfg");
+    }
+}
+module.exports = { Person };
--- build/router/somethingroute.js
+++ build/router/somethingroute.js
@@ -4,10 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const classmodulewithjs_1 = require("../helper/classmodulewithjs");
 const classmodule_1 = __importDefault(require("../helper/classmodule"));
 const someThingRoute = express_1.default.Router();
 someThingRoute.get('/something', (req, res) => {
     console.log(classmodule_1.default);
+    let person = new classmodulewithjs_1.Person("sdfsdf");
+    console.log(person);
     console.log("something1");
     console.log("something2");
     console.log("something 3");
@@ -15,4 +18,5 @@ someThingRoute.get('/something', (req, res) => {
 });
 console.log("dev");
 console.log("dev");
+console.log(classmodulewithjs_1.Person.method());
 exports.default = someThingRoute;
